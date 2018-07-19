50903b40c04b6819226704c554b31cf5faac1fcc
prashanth <kamuluprashanth@gmail.com>
2018-07-19 12:06:10 +0530

    initial commit1

---
+++ build/release/src/CMakeFiles/CryptoNoteCore.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/var/www/html/jyocoin/src/CryptoNoteCore/Account.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Account.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/BlockIndex.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockIndex.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/Blockchain.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Blockchain.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/BlockchainIndices.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainIndices.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/BlockchainMessages.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainMessages.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/Checkpoints.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Checkpoints.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/Core.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Core.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/CoreConfig.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CoreConfig.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/CryptoNoteBasic.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteBasic.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/CryptoNoteBasicImpl.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteBasicImpl.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/CryptoNoteFormatUtils.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteFormatUtils.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/CryptoNoteSerialization.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteSerialization.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/CryptoNoteTools.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteTools.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/Currency.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Currency.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/Difficulty.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Difficulty.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/IBlock.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/IBlock.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/ITimeProvider.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/ITimeProvider.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/Miner.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Miner.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/MinerConfig.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/MinerConfig.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/SwappedMap.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/SwappedMap.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/SwappedVector.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/SwappedVector.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/Transaction.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Transaction.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/TransactionExtra.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionExtra.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/TransactionPool.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionPool.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/TransactionPrefixImpl.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionPrefixImpl.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/TransactionUtils.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionUtils.cpp.o"
+  "/var/www/html/jyocoin/src/CryptoNoteCore/UpgradeDetector.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/UpgradeDetector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  "UPNP_DYNAMIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/release/src/CMakeFiles/Rpc.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/var/www/html/jyocoin/src/Rpc/HttpClient.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/Rpc.dir/Rpc/HttpClient.cpp.o"
+  "/var/www/html/jyocoin/src/Rpc/HttpServer.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/Rpc.dir/Rpc/HttpServer.cpp.o"
+  "/var/www/html/jyocoin/src/Rpc/JsonRpc.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/Rpc.dir/Rpc/JsonRpc.cpp.o"
+  "/var/www/html/jyocoin/src/Rpc/RpcServer.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/Rpc.dir/Rpc/RpcServer.cpp.o"
+  "/var/www/html/jyocoin/src/Rpc/RpcServerConfig.cpp" "/var/www/html/jyocoin/build/release/src/CMakeFiles/Rpc.dir/Rpc/RpcServerConfig.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  "UPNP_DYNAMIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
